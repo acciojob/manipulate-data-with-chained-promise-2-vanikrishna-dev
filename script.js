5da4a80c0bb2618f3e7797d1eb9a2123d7eef7ac
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-28 15:04:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+function manipulateArray() {
+  const arr = [1, 2, 3, 4];
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  });
+}
+
+manipulateArray()
+
+  .then((data) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const evenNumbers = data.filter((num) => num % 2 === 0);
+        output.innerText = evenNumbers.toString();
+        resolve(evenNumbers);
+      }, 1000);
+    });
+  })
+
+  .then((evenNumbers) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const doubled = evenNumbers.map((num) => num * 2);
+        output.innerText = doubled.toString();
+        resolve(doubled);
+      }, 2000);
+    });
+  });
